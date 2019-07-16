6e2e52d387bef25eb363355d63f2ae23cdfd14c2
aa208349 <a208349@163.com>
2019-07-16 19:11:48 +0800

    添加大转盘后台

--- application/admin/controller/Turntable.php
+++ application/admin/controller/Turntable.php
@@ -13,19 +13,40 @@ class Turntable extends Common
     public function index()
     {
         $list = array();
-        $list = Db::name('order')->field('box_id,order_id,overdue_time,lottery_time')->where('order_type',2)->where('pay_status',1)->paginate(10);
+        $list = Db::name('order')->field('box_id,order_id,overdue_time,lottery_time')->where('order_type',2)->where('pay_status',1)->paginate(10)->each(function($v,$k){
+            $v['goods_num'] = Db::name('order_goods')->where('order_id',$v['order_id'])->value('goods_num');
+            return $v;
+        });
         $this->assign('list',$list);
         return $this->fetch();
     }
 
-    //edit
+    //设置中奖名单
     public function edit()
     {   
-        if(Request::instance()->isPost()){
-            $ids = input('ids');
-        }
         $order_id = input('order_id',0);
-        $list = Db::name('gift_order_join')->alias('g')->join('member m','g.user_id=m.id')->where('order_id',$order_id)->select();
+        if(Request::instance()->isPost()){
+            $post = input('post.');
+            $ids = $post['ids'];
+            $goods_num = Db::name('order_goods')->where('order_id',$order_id)->value('goods_num');
+            $end_time = Db::name('order')->where('order_id',$order_id)->value('overdue_time');
+            if(time() >= $end_time){
+                $this->error('群抢时间已结束，不能设置中奖名单');
+            }
+            if(!$ids){
+                $this->error('请勾选中奖的名单');
+            }
+            if(count($ids)>$goods_num){
+                $this->error('中奖人数不能大于购买数量');
+            }
+            $res = Db::name('gift_order_join')->where('order_id',$order_id)->where('user_id','in',$ids)->update(['status'=>1]);
+            if($res){
+                $this->success('设置中奖名单成功',url('index'));
+            }else{
+                $this->error('设置中奖名单失败');
+            }
+        }
+        $list = Db::name('gift_order_join')->alias('g')->field('m.nickname,m.id,g.status')->join('member m','g.user_id=m.id')->where('g.order_id',$order_id)->where('m.nickname','neq','')->select();
         $this->assign('list',$list);
         $this->assign('order_id',$order_id);
         return $this->fetch();
