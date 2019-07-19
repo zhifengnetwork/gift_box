e178799b702b6812390ecdb080a8483378c6e316
FZJ <yudwcom@163.com>
2019-07-19 19:58:05 +0800

    card

---
+++ application/card/controller/Index.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\card\controller;
+
+use think\Controller;
+
+class Index extends Controller
+{
+    public function index()
+    {
+    
+        
+        return $this->fetch();
+    }
+}
