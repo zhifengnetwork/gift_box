b95d7986ac37c3cfb7dd77db21c3c04d251eb7ce
aa208349 <a208349@163.com>
2019-07-20 11:42:57 +0800

    g

--- application/api/controller/Goods.php
+++ application/api/controller/Goods.php
@@ -692,7 +692,7 @@ class Goods extends ApiBase
 
         $data = Db::table('goods')->alias('g')
         ->join('goods_attr ga','FIND_IN_SET(ga.id,g.goods_attr)','LEFT')
-        ->field('g.*,GROUP_CONCAT(ga.name) attr_name')
+        ->field('g.goods_id,g.goods_name,g.price,g.goods_spec,g.desc,g.content,g.goods_attr,GROUP_CONCAT(ga.name) attr_name')
         ->where('g.is_show',1)
         ->find($goods_id);
         $data['picture'] = Db::name('goods_img')->where('goods_id',$goods_id)->where('main',1)->value('picture');
