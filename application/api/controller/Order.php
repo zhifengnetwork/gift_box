fb471e9a27d9ac983c24dcf5c6485c3db8a151ac
aa208349 <a208349@163.com>
2019-07-24 14:23:53 +0800

    g

--- application/api/controller/Order.php
+++ application/api/controller/Order.php
@@ -585,7 +585,7 @@ class Order extends ApiBase
                         ->join('goods g','g.goods_id=og.goods_id','LEFT')
                         ->join('gift_order_join goj','o.order_id=goj.order_id','LEFT')
                         ->where($where)
-                        ->whereor($whereor)
+                        // ->whereor($whereor)
                         ->group('og.order_id')
                         ->order('o.order_id DESC')
                         ->field('o.order_id,o.add_time,o.order_sn,og.goods_name,gi.picture img,og.spec_key_name,og.goods_price,g.original_price,og.goods_num,o.order_status,o.pay_status,o.shipping_status,o.pay_type,o.parent_id,o.total_amount,o.shipping_price,o.order_type,o.lottery_time,o.giving_time,o.overdue_time,o.gift_uid')
