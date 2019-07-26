0e6c39935c942e64d7c585057c3a5f3cea7eca4a
luyu627 <152939118@qq.com>
2019-07-26 15:32:59 +0800

    优化开奖时间未到

--- application/api/controller/Gift.php
+++ application/api/controller/Gift.php
@@ -344,8 +344,8 @@ class Gift extends ApiBase
 
     //转动转盘
     public function turn_the_wheel(){
-        $user_id = $this->get_user_id();
-        $order_id = input('order_id',0);
+        $user_id = 86;//$this->get_user_id();
+        $order_id = 2835;//input('order_id',0);
         if(!$user_id){
             $this->ajaxReturn(['status' => -1 , 'msg'=>'用户不存在','data'=>'']);
         }
@@ -362,11 +362,13 @@ class Gift extends ApiBase
             $this->ajaxReturn(['status' => -1 , 'msg'=>'您已经参与过此次抽奖','data'=>'']);
 
         $order = Db::name('order')->field('order_status,shipping_status,pay_status,parent_id,order_type,lottery_time,giving_time,overdue_time,gift_uid')->where(['order_id'=>$order_id])->find();
+		echo $order['lottery_time'] , '<br />';
+		echo time();
         if(!$order['lottery_time']){
             $this->ajaxReturn(['status' => -1 , 'msg'=>'该订单已不能抽奖！','data'=>'']);
-        }elseif(!$order['lottery_time'] < time())
-            $this->ajaxReturn(['status' => -1 , 'msg'=>'该订单已不能抽奖！','data'=>'']);
-        elseif(!$order['overdue_time'] < time())
+        }elseif($order['lottery_time'] > time())
+            $this->ajaxReturn(['status' => -1 , 'msg'=>'开奖时间未到！','data'=>'']);
+        elseif($order['overdue_time'] < time())
             $this->ajaxReturn(['status' => -1 , 'msg'=>'该订单已不能抽奖！','data'=>'']);            
 
         //有人转动转盘，则给此次群抢全部设置开奖用户
