8bcec55955149eb9cfc2d933ff961ddfbe85596b
aa208349 <a208349@163.com>
2019-07-26 10:17:41 +0800

    ggg

--- application/card/controller/Index.php
+++ application/card/controller/Index.php
@@ -11,17 +11,21 @@ class Index extends Controller
     {
         $card_id = input('card_id',0);
         $info = Db::name('box')->where('id',$card_id)->find();
-        $info['cate_url'] = '';
-        $info['music_url'] = '';
-        $info['video_url'] = '';
-        $info['scene_url'] = '';
-        if(!$card_id){
+        if(!$info){
+            $info['cate_url'] = '';
+            $info['music_url'] = '';
+            $info['video_url'] = '';
+            $info['scene_url'] = '';
             $info['photo_url'] = '';
             $info['content'] = '';
             $info['voice_url'] = '';
             $this->assign('info',$info);
             return $this->fetch();
         }
+        $info['cate_url'] = '';
+        $info['music_url'] = '';
+        $info['video_url'] = '';
+        $info['scene_url'] = '';
         //类别
         if($info['cate_id']){
             $info['cate_url'] = Db::name('box_cate')->where('id',$info['cate_id'])->value('picture');
