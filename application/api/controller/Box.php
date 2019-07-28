6a3f591142f7a1b07ae1cfa61e289d2d30f57fb3
lmp5023 <670600971@qq.com>
2019-07-28 18:12:25 +0800

    share_log

--- application/api/controller/Box.php
+++ application/api/controller/Box.php
@@ -230,4 +230,15 @@ class Box extends ApiBase
         }
     }
 
+
+    /**
+     * 小程序 分享 记录
+     */
+    public function share_log(){
+        $url = I('url');
+        $ur = urldecode($url);
+        write_log($content);
+        $this->ajaxReturn(['status' => 1 , 'msg'=>'log记录成功','data'=>'']);
+    }
+
 }
