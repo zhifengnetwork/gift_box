33fd9fbc04077f5458735dcc449648fb964f490a
aa208349 <a208349@163.com>
2019-07-29 10:33:10 +0800

    ‘ggg

--- application/admin/controller/Sharing.php
+++ application/admin/controller/Sharing.php
@@ -14,9 +14,29 @@ class Sharing extends Common
      */
     public function index()
     {
-        $list  = Db::name('sharing_circle')->paginate(10);
+        $list  = Db::name('sharing_circle')->order('addtime desc')->paginate(10);
         $this->assign('list',$list);
         return $this->fetch();
     }
 
+    //添加话题
+    public function add_topic()
+    {
+        $id = input('id');
+        if($id){
+            $info = Db::name('sharing_topic')->where('id',$id)->find();
+        }else{
+            $info = getTableField('sharing_topic');
+        }
+        $this->assign('info',$info);
+        return $this->fetch();
+    }
+    
+    //话题列表
+    public function topic_list()
+    {
+        $list  = Db::name('sharing_topic')->order('addtime desc')->paginate(10);
+        $this->assign('list',$list);
+        return $this->fetch();
+    }
 }
