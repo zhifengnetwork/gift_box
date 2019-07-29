9206d2e0c1ae45dc9153ae94ffd990e5ca480323
aa208349 <a208349@163.com>
2019-07-29 19:44:58 +0800

    ggg

--- application/cron/controller/Team.php
+++ application/cron/controller/Team.php
@@ -71,7 +71,7 @@ class Team extends Controller{
     }    
 
     private function set_gift_time($Order,$GiftOrderJoin,$v){
-        $info = $GiftOrderJoin->field('id,address_id')->where(['order_id'=>$v['order_id'],'order_type'=>order_type,'status'=>1,'join_status'=>['neq',4]])->find();
+        $info = $GiftOrderJoin->field('id,address_id')->where(['order_id'=>$v['order_id'],'order_type'=>$order_type,'status'=>1,'join_status'=>['neq',4]])->find();
         //已过期且没填地址
         if(($v['overdue_time'] < time()) && !$info['address_id']){ 
             // 启动事务
