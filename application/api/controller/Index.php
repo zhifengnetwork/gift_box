4c9f4a1acd48c82646e9188c20d16b233814609f
aa208349 <a208349@163.com>
2019-08-20 19:29:20 +0800

    g

--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -45,7 +45,7 @@ class Index extends ApiBase
             $goods_info2 = array();
         }
 
-        $goods_list3 = Db::table('goods')->alias('g')->join('goods_img i','g.goods_id=i.goods_id','LEFT')->field('g.goods_id,g.goods_name,g.price,i.picture')->where(['goods_attr1'=>3,'g.is_recommend'=>1,'is_del'=>0,'is_show'=>1,'i.main'=>1])->order('add_time desc')->limit(3)->select();
+        $goods_list3 = Db::table('goods')->alias('g')->join('goods_img i','g.goods_id=i.goods_id','LEFT')->field('g.goods_id,g.goods_name,g.price,i.picture')->where(['goods_attr1'=>3,'g.is_recommend'=>1,'is_del'=>0,'is_show'=>1,'i.main'=>1])->order('add_time desc')->limit(8)->select();
 
         $goods_list4 = Db::table('goods')->alias('g')->join('goods_img i','g.goods_id=i.goods_id','LEFT')->field('g.goods_id,g.goods_name,g.price,i.picture,g.picture as sy_picture')->where(['goods_attr1'=>4,'g.is_recommend'=>1,'is_del'=>0,'is_show'=>1,'i.main'=>1])->order('add_time desc')->limit(8)->select();
         foreach($goods_list4 as $key=>$val){
