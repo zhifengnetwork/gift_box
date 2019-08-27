d5ec364dab1f1db1c4a051a5237b4d50849e35e5
aa208349 <a208349@163.com>
2019-08-27 16:30:07 +0800

    update

--- application/admin/controller/Box.php
+++ application/admin/controller/Box.php
@@ -48,8 +48,8 @@ class Box extends Common
             $info['nickname'] = Db::table('member')->where('id',$info['user_id'])->value('nickname');
             $info['music_name'] = Db::table('box_music')->where('id',$info['music_id'])->value('name');
             $info['video_name'] = Db::table('box_video')->where('id',$info['video_id'])->value('name');
-            $info['scene_name'] = Db::table('box_scene')->where('id',$info['scene_id'])->value('name');
-            $info['cate_name'] = Db::table('box_cate')->where('id',$info['cate_id'])->value('name');
+            $info['scene_name'] = Db::table('box_scene')->where('id',$info['cate_id'])->value('name');
+            // $info['cate_name'] = Db::table('box_cate')->where('id',$info['cate_id'])->value('name');
             $this->assign('info',$info);
         }else{
             $this->error('礼盒不存在');
