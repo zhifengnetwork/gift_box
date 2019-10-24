ab88f18fb68e0e2eb39c27410a295df1271e56ad
fuckdajiba <1344792562@qq.com>
2019-10-24 18:06:39 +0800

    user upload diy background

---
+++ application/api/controller/Xs.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace app\api\controller;
+
+class Xs extends ApiBase
+{
+    /**
+     * describe:上传个人中心背景图片
+     * method:post
+     * user:xs
+     * $param user_id
+     * $param file
+     */
+    public function upload(){
+
+        $user_id = request()->param('user_id');
+        // 获取表单上传文件 例如上传了001.jpg
+        $file = request()->file('file');
+        // 移动到框架应用根目录/public/uploads/ 目录下
+        if(!$file){
+            echo '图片上传有误';die;
+        }
+        $info = $file->validate(['ext'=>'jpg,png,gif'])->move(ROOT_PATH . 'public' . DS . 'uploads');
+        if($info){
+            //成功上传后 获取上传信息
+            //输出 jpg
+            //echo $info->getExtension();
+            //输出 20160820/42a79759f284b767dfcb2a0197904287.jpg
+            //echo $info->getSaveName();
+            //输出 42a79759f284b767dfcb2a0197904287.jpg
+            //echo $info->getFilename();
+            //echo $info->pathName;
+            //获取图片的存放相对路径
+            $filePath = 'public' . DS . 'uploads'.DS .$info->getSaveName();
+            $getInfo = $info->getInfo();
+            //获取图片的原名称
+            $name = $getInfo['name'];
+            //整理数据,写入数据库
+            $data = [
+                'user_id' => $user_id,
+                'diy_background' => $filePath,
+                'add_time' => time(),
+//                date('Y-m-d H:i:s')
+            ];
+            // 插入数据库
+            $res = \think\Db::name('user_diy_background')->where(['user_id'=>$user_id])->find();
+            if($res){
+            $affected = \think\Db::name('user_diy_background')->where(['user_id'=>$user_id])->update($data);
+            }elseif (!$res){
+            $affected = \think\Db::name('user_diy_background')->insert($data);
+            }
+        }else{
+            // 上传失败获取错误信息
+            echo $file->getError();
+        }
+    }
+}
